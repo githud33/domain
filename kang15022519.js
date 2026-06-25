e2838969f0f8d1de4f7a6e155b883b667d98c735
githud33 <githud33@gmail.com>
2026-06-25 08:10:01 +0700

    Update kang15022519.js

--- kang15022519.js
+++ kang15022519.js
@@ -1,4 +1,4 @@
-// 1. ตั้งค่าโดเมนส่วนกลางที่จุดเดียวเหมือนเดิม
+// 1. ตั้งค่าโดเมนส่วนกลางสำหรับ video ที่จุดเดียวเหมือนเดิม
 var myDomain = "https://kang1502.github.io/Series/"; 
 
 (function() {
@@ -7,4 +7,17 @@ var myDomain = "https://kang1502.github.io/Series/";
         var currentFile = sourceTag.getAttribute('src');
         sourceTag.src = myDomain + currentFile;
     }
+})();
+
+// ตั้งค่าโดเมนส่วนกลางสำหรับเก็บซับไตเติล (เวลาโดเมนเปลี่ยน มาแก้ตรงนี้ที่เดียวจบ!)
+var subDomain = "https://kang1502.github.io/subtitle/";
+
+(function() {
+    var trackTags = document.querySelectorAll('track');
+    trackTags.forEach(function(track) {
+        var currentFile = track.getAttribute('src');        
+        if (currentFile) {
+            track.src = subDomain + currentFile;
+        }
+    });
 })();
