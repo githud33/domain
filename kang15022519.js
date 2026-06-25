4990aab37342517d8cd7bf8b228d42d5402545c4
githud33 <githud33@gmail.com>
2026-06-25 07:13:38 +0700

    Update kang15022519.js

--- kang15022519.js
+++ kang15022519.js
@@ -1,16 +1,10 @@
 // 1. ตั้งค่าโดเมนส่วนกลางที่จุดเดียวเหมือนเดิม
-var videoDomain = "https://kang1502.github.io/Series/";
+var myDomain = "https://kang1502.github.io/Series/"; 
 
 (function() {
-    // ค้นหาแท็ก source ที่มี id="mp" บนหน้าเว็บ
-    var mpSource = document.getElementById('m3');
-    
-    if (mpSource) {
-        // ดึงค่าไฟล์เดิมที่ใส่ไว้ (เช่น ดึงคำส่วหลัง ออกมา)
-        var currentFile = mpSource.getAttribute('src');
-        
-        // เอาโดเมนใหม่ไปต่อข้างหน้าไฟล์เดิม
-        mpSource.src = videoDomain + currentFile;
-        
+    var sourceTag = document.getElementById('m3'); 
+    if (sourceTag) {
+        var currentFile = sourceTag.getAttribute('src');
+        sourceTag.src = myDomain + currentFile;
     }
 })();
