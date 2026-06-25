218575f0efbc726d488c82a47c284382f8607456
githud33 <githud33@gmail.com>
2026-06-25 14:51:13 +0700

    Create rumble.com.js

---
+++ rumble.com.js
@@ -0,0 +1,10 @@
+// 1. ตั้งค่าโดเมนส่วนกลางที่จุดเดียวเหมือนเดิม
+var myDomain = "https://hugh.cdn.rumble.cloud/"; // ใช้ชื่อ myDomain
+
+(function() {
+    var sourceTag = document.getElementById('ru'); // ใช้ชื่อ sourceTag
+    if (sourceTag) {
+        var currentFile = sourceTag.getAttribute('src');
+        sourceTag.src = myDomain + currentFile;
+    }
+})();
