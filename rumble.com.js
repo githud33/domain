eb0bda855403819ce570f5d97d4e0612e5d13afd
githud33 <githud33@gmail.com>
2026-06-25 15:15:04 +0700

    Update rumble.com.js

--- rumble.com.js
+++ rumble.com.js
@@ -1,5 +1,5 @@
 // 1. ตั้งค่าโดเมนส่วนกลางที่จุดเดียวเหมือนเดิม
-var myDomain = "https://rumble.com/"; // ใช้ชื่อ myDomain
+var myDomain = "https://rumble.com/hls-vod/"; // ใช้ชื่อ myDomain
 
 (function() {
     var sourceTag = document.getElementById('ru'); // ใช้ชื่อ sourceTag
