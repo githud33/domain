5d5a240bc55bca342ee9842387755ccc86fafd7d
githud33 <githud33@gmail.com>
2026-08-02 16:44:54 +0700

    Update rumble.com.js

--- rumble.com.js
+++ rumble.com.js
@@ -8,3 +8,16 @@ var myDomain = "https://rumble.com/hls-vod/"; // ใช้ชื่อ myDomain
         sourceTag.src = myDomain + currentFile;
     }
 })();
+
+// =============================================================================
+// =============================================================================
+// 2. ตัวเปิดเล่นชั่วคราว
+var myDomain = "https://rumble.com/hls-vod/"; // ใช้ชื่อ myDomain
+
+(function() {
+    var sourceTag = document.getElementById('ru3'); // ใช้ชื่อ sourceTag
+    if (sourceTag) {
+        var currentFile = sourceTag.getAttribute('src');
+        sourceTag.src = myDomain + currentFile;
+    }
+})();
