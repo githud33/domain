56c2022c20879b515a63103c781cf488301dc101
githud33 <githud33@gmail.com>
2026-08-02 16:49:44 +0700

    Update cdn.rumble.js

--- cdn.rumble.js
+++ cdn.rumble.js
@@ -9,3 +9,17 @@ var myDomain = "https://1a-1791.com/video/"; // ใช้ชื่อ myDomain
         sourceTag.src = myDomain + currentFile;
     }
 })();
+
+// =============================================================================
+// =============================================================================
+// 2. ตัวเปิดเล่นชั่วคราว
+//  https://hugh.cdn.rumble.cloud/video/
+var myDomain = "https://1a-1791.com/video/"; // ใช้ชื่อ myDomain
+
+(function() {
+    var sourceTag = document.getElementById('mp4'); // ใช้ชื่อ sourceTag
+    if (sourceTag) {
+        var currentFile = sourceTag.getAttribute('src');
+        sourceTag.src = myDomain + currentFile;
+    }
+})();
