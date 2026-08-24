f3ca210e732b6b918b0b0997a5a73bab0c60b233
githud33 <githud33@gmail.com>
2026-08-24 12:13:13 +0700

    Update cdn.rumble.js

--- cdn.rumble.js
+++ cdn.rumble.js
@@ -1,6 +1,7 @@
 // 1. ตั้งค่าโดเมนส่วนกลางที่จุดเดียวเหมือนเดิม
 //  https://hugh.cdn.rumble.cloud/video/
-var myDomain = "https://1a-1791.com/video/"; // ใช้ชื่อ myDomain
+// https://1a-1791.com/video/
+var myDomain = "https://hugh.cdn.rumble.cloud/video/"; // ใช้ชื่อ myDomain
 
 (function() {
     var sourceTag = document.getElementById('mp'); // ใช้ชื่อ sourceTag
